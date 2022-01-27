0433207f98ed2bd8bc2c28b102d98a56d6dc2060
filbertmsaki <msakifil111@gmail.com>
2022-01-27 21:45:16 +0300

    V1.0.0 Final Changes

--- src/NextSmsServiceProvider.php
+++ src/NextSmsServiceProvider.php
@@ -5,8 +5,6 @@ use Illuminate\Support\ServiceProvider;
 
 class NextSmsServiceProvider extends ServiceProvider{
     public function boot(){
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-        $this->loadViewsFrom(__DIR__.'/views','nextsms');
         $this->mergeConfigFrom(__DIR__.'/config/nextsms.php', 'nextsms');
         $this->publishes([__DIR__.'/config/nextsms.php' => config_path('nextsms.php'),]);
 
